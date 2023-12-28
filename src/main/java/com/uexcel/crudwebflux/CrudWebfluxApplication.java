8c11a3c464469561ceb3729dc2dafd98d84f3b2e
Uexcel2022 <excellenceudoka@gmail.com>
2023-12-28 14:39:34 +0100

    commit

---
+++ src/main/java/com/uexcel/crudwebflux/CrudWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.uexcel.crudwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudWebfluxApplication.class, args);
+    }
+
+}
